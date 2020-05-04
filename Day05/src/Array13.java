f95e7f0cfbab8fc8d340e412d0c768c82dbd0250
aseongen <62504646+aseongen@users.noreply.github.com>
2020-05-04 23:18:22 +0900

    Apr2920 5_Java_Practice

---
+++ Day05/src/Array13.java
@@ -0,0 +1,26 @@
+
+public class Array13 {
+	public static void main(String[] args) {
+
+    int [] score = { 100,90,80}; //배열 원소값을 직접 저장하면서 score 배열생성
+    int sum =0;
+    
+    for(int i=0; i<score.length;i++) {
+    	System.out.println("score[" + i + "] : " + score[i]);
+    }
+    
+    for (int k : score) {
+    	sum= sum +k; // 누적합
+    }
+   System.out.println("배열원소 누적합= " + sum);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
